b25c33ce710a4b3cb432abfa4f9963188823a0c3
leewebstechnologies.ali <leewebstechnologies.aliuajibade317@gmail.com>
2026-07-30 22:33:25 -0700

    remove blog

---
+++ app/blog/blog-detail/page.tsx
@@ -0,0 +1,8 @@
+import "./blog-detail.css";
+
+const BlogDetail = () => {
+  return (
+    <div>BlogDetail</div>
+  )
+}
+export default BlogDetail
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,47 +1,8 @@
-import Link from "next/link";
-import Image from "next/image";
-import "./blogCard.css";
+import "./blog.css";
 
-type Blog = {
-  slug: string;
-  title: string;
-  image: string;
-  author: string;
-  date: string;
-  category: string;
-  excerpt: string;
-};
-
-type BlogCardProps = {
-  blog: Blog;
-};
-
-const BlogCard = ({ blog }: BlogCardProps) => {
+const Blog = () => {
   return (
-    <article className="single-blog mt-30">
-      <div className="blog-thumb">
-        <Image src={blog.image} alt={blog.title} width={750} height={450} />
-      </div>
-
-      <div className="blog-content">
-        <h4>
-          <Link href={`/blog/${blog.slug}`}>{blog.title}</Link>
-        </h4>
-
-        <ul className="blog-meta">
-          <li>{blog.author}</li>
-          <li>{blog.date}</li>
-          <li>{blog.category}</li>
-        </ul>
-
-        <p>{blog.excerpt}</p>
-
-        <Link href={`/blog/${blog.slug}`} className="main-btn">
-          Read More
-        </Link>
-      </div>
-    </article>
-  );
-};
-
-export default BlogCard;
+    <div>Blog</div>
+  )
+}
+export default Blog
